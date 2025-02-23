211e2f1f8a1943f83344b8346c654d9497c2947d
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2025-02-23 19:14:39 +0100

    wip: Add REST endpoints
    Also adds the necessary service abstraction which will be coming up when creating a new product.

---
+++ backend/src/main/java/com/github/dannil/productmanagement/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.github.dannil.productmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.dannil.productmanagement.model.database.Product;
+import com.github.dannil.productmanagement.model.dto.ProductDto;
+import com.github.dannil.productmanagement.service.ProductService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<ProductDto>> getAll() {
+        List<ProductDto> dtos = this.productService.findAll().stream().map(Product::toFlatView).toList();
+        return ResponseEntity.ok(dtos);
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<ProductDto> getById(@PathVariable Long id) {
+        Optional<Product> product = this.productService.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get().toNormalView());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
